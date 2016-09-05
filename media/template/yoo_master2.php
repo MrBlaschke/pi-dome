94651867b008a1be13ef645512d54003fc0f50ac
Frank Price <github@mrblaschke.de>
2016-09-05 09:27:54 +0200

    erster commit piDome

---
+++ media/template/yoo_master2.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "8132abc6b83544b85126bef9bca125c4",
+    "tmpl_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}",
+    "warp_check": "6b8151df73a9f2a91e8f0e2e1fa28875",
+    "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.28 is currently the newest version available.\"}"
+}
