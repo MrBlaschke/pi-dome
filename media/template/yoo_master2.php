e10a1cc8909f9085ec9626e9d0b516e2e94b6a3d
Frank Price <github@mrblaschke.de>
2016-09-06 13:49:22 +0200

    anpassungen für beta 1

--- media/template/yoo_master2.php
+++ media/template/yoo_master2.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "5663ae4fc5eb347de0604f6f1a9f4543",
+    "tmpl_check": "4271040cd3239f700a77bc3af816911a",
     "tmpl_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}",
-    "warp_check": "1d9135f11aa54c25653e9a6fea89478f",
+    "warp_check": "6d7b38b925a25921d811b559268f88ac",
     "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.28 is currently the newest version available.\"}"
 }
